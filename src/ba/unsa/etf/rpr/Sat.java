09c4ec2a7730539f518fec86af8ff966b1c7a256
Hadzibajramovic Amila <ahadzibajr1@etf.unsa.ba>
2020-10-26 13:10:11 +0100

    Dodan konstruktor klase Sat i metoda Postavi

--- src/ba/unsa/etf/rpr/Sat.java
+++ src/ba/unsa/etf/rpr/Sat.java
@@ -3,6 +3,13 @@ package ba.unsa.etf.rpr;
 public class Sat {
     private int sati,minute,sekunde;
 
+    public Sat (int sati, int minute, int sekunde){
+        postavi(sati,minute,sekunde);
+    }
+    public void postavi (int sati, int minute, int sekunde){
+        this.sati = sati; this.minute = minute; this.sekunde=sekunde;
+    }
+
 
 
 
